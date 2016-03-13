12b04116485f3fcbff9fe25b6c18d0490f8ed162
Sergey Feschukov <dobroweb@users.noreply.github.com>
2016-03-13 19:07:40 +0500

    Added new option `modvkmarket_desc_field`

--- core/components/modvkmarket/lexicon/ru/setting.inc.php
+++ core/components/modvkmarket/lexicon/ru/setting.inc.php
@@ -3,6 +3,7 @@
 $_lang['area_modvkmarket_main'] = 'Основные';
 $_lang['area_modvkmarket_vksetting'] = 'Настроки VK';
 
+$_lang['setting_modvkmarket_desc_field'] = 'Поле для описания товара';
 $_lang['setting_modvkmarket_goods_clear_limit'] = 'Лимит очистки за итерацию';
 $_lang['setting_modvkmarket_goods_sync_limit'] = 'Сколько товаров добавлять за раз';
 $_lang['setting_modvkmarket_shop_type'] = 'Тип магазина';
